dd7b1395da896cd73e717501756682f049b10f56
DESKTOP-H2HN5RC\namut <namukuri@gmail.com>
2025-03-10 19:12:46 +0900

    士官データ表示途中

---
+++ Assets/Scripts/OfficerRank.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum OfficerRank
+{ 
+    ­ˆÑ,
+    ’†ˆÑ,
+    ‘åˆÑ,
+    ­²,
+    ’†²,
+    ‘å²,
+    ­«,
+    ’†«,
+    ‘å«,
+    Œ³ƒ,
+    ‘‰¤
+}
---
+++ Assets/Scripts/V_OfficerData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class V_OfficerData
+{
+    public int v_officerNo;
+    public string v_OfficerName;
+    public string v_OfficerStatus;
+    public Sprite v_OfficerSprite;
+    public OfficerRank officerRank;
+    public int meritValue;
+    public int attackPower;
+    public int defensePower;
+
+}
